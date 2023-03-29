d212ec12b2de869cec2c220e624f89722e2e63fe
Anirudh Gunde <anirudh.gunde1@gmail.com>
2023-03-29 13:47:56 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Anirudh Gunde' });
+  res.render('mydata', { title: 'Anirudh Gunde' });
 });
 
 module.exports = router;
